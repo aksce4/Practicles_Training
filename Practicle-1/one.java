1e6a4b7232c86de8bc1e5fdb52edb3e448a54525
aksce3 <aksce3@gmail.com>
2018-07-03 11:52:15 +0530

    Practicle-1

---
+++ Practicle-1/one.java
@@ -0,0 +1,7 @@
+class one{
+    public static void main(String args[]){
+       String st1 = "I am in Ahmedabad. Ahmedabad is in Gujarat.";
+       int id = st1.lastIndexOf("Ahmedabad");
+       System.out.println(id);
+    }
+}
---
+++ Practicle-1/one.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>){
+    var st: String = "I am in Ahmedabad. Ahmedabad is in Gujarat."
+    var id: Int = st.lastIndexOf("Ahmedabad")
+    println(id)
+}
