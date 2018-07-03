3c36353be3061a0d20526dc6ba904aeeba7e7768
aksce3 <aksce3@gmail.com>
2018-07-03 18:34:34 +0530

    swipable tab

---
+++ Practicle-6/Practicle_swipable_tablist/app/src/main/java/com/example/akshayparmar/project3/fragment1.java
@@ -0,0 +1,25 @@
+package com.example.akshayparmar.project3;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class fragment1 extends Fragment {
+    private static final String TAG = "Tab1Fragment";
+
+    private Button btnTEST;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        return view;
+    }
+
+}
---
+++ Practicle-6/Practicle_swipable_tablist/app/src/main/java/com/example/akshayparmar/project3/fragment2.java
@@ -0,0 +1,25 @@
+package com.example.akshayparmar.project3;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class fragment2 extends Fragment {
+    private static final String TAG = "Tab2Fragment";
+
+    private Button btnTEST;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main2, container, false);
+
+        return view;
+    }
+
+}
